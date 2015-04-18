3de9f7f19fca707548405028666f5199c190ec95
poilon <poilon@gmail.com>
2015-04-18 10:54:10 +0200

    textures

--- lib/models/brick.rb
+++ lib/models/brick.rb
@@ -8,15 +8,48 @@ class Brick
     @y1 = params[:y1]
     @y2 = params[:y2]
     @color = params[:color]
+    @texture = params[:texture]
+    @texture_size_x = params[:texture_size_x]
+    @texture_size_y = params[:texture_size_y]
   end
 
   def draw(window)
     @color ||= Gosu::Color::AQUA
-    window.draw_quad(
-      window.scroll_x + x1, GameWindow::HEIGHT - y1, @color,
-      window.scroll_x + x1, GameWindow::HEIGHT - y2, @color,
-      window.scroll_x + x2, GameWindow::HEIGHT - y2, @color,
-      window.scroll_x + x2, GameWindow::HEIGHT - y1, @color,
-    )
+    if @texture
+      @sprite ||= Gosu::Image.load_tiles(window, "media/#{@texture}.png", @texture_size_x, @texture_size_y, true)
+      texture = @sprite[0]
+      draw_texture(window, texture, @texture_size_x, @texture_size_y)
+    else
+      window.draw_quad(
+        window.scroll_x + x1, GameWindow::HEIGHT - y1, @color,
+        window.scroll_x + x1, GameWindow::HEIGHT - y2, @color,
+        window.scroll_x + x2, GameWindow::HEIGHT - y2, @color,
+        window.scroll_x + x2, GameWindow::HEIGHT - y1, @color,
+      )
+    end
   end
+
+  def draw_texture(window, texture, size_x, size_y)
+    @white ||= Gosu::Color::WHITE
+    ((y2 - y1) / size_y).times do |current_y|
+      ((x2 - x1) / size_x).times do |current_x|
+        texture.draw_as_quad(
+          window.scroll_x + x1 + current_x * size_x,
+          GameWindow::HEIGHT - y2 + current_y * size_y,
+          @white,
+          window.scroll_x + x1 + current_x * size_x,
+          GameWindow::HEIGHT - y2 + (current_y + 1) * size_y,
+          @white,
+          window.scroll_x + x1 + (current_x + 1) * size_x,
+          GameWindow::HEIGHT - y2 + (current_y + 1) * size_y,
+          @white,
+          window.scroll_x + x1 + (current_x + 1) * size_x,
+          GameWindow::HEIGHT - y2 + current_y * size_y,
+          @white,
+          0
+        )
+      end
+    end
+  end
+
 end
--- lib/models/game_map.rb
+++ lib/models/game_map.rb
@@ -9,12 +9,12 @@ class GameMap
     ]
     @bricks = [
       #Brick.new(self, x1: -10000, x2: 10000, y1: -10000, y2: 10000, color: Gosu::Color::GRAY),
-      Brick.new(self, x1: -1000, x2: 20, y1: -100, y2: 100),
-      Brick.new(self, x1: -1000, x2: 5000, y1: 0, y2: 10),
-      Brick.new(self, x1: 1000, x2: 1100, y1: 0, y2: 20),
-      Brick.new(self, x1: -1000, x2:-500, y1:0, y2:500),
-      Brick.new(self, x1: 100, x2: 1000, y1: 150, y2: 520),
-      Brick.new(self, x1: 600, x2: 720, y1: 120, y2: 300)
+      Brick.new(self, x1: -1004, x2: 20, y1: -100, y2: 100, texture: 'ground', texture_size_x: 32, texture_size_y: 32),
+      Brick.new(self, x1: -1000, x2: 5400, y1: -22, y2: 10, texture: 'ground', texture_size_x: 32, texture_size_y: 32),
+      Brick.new(self, x1: 1000, x2: 1128, y1: 0, y2: 32, texture: 'fire', texture_size_x: 32, texture_size_y: 32),
+      Brick.new(self, x1: -1000, x2:-500, y1:0, y2: 500),
+      Brick.new(self, x1: 100, x2: 1000, y1: 150, y2: 534, texture: 'block', texture_size_x: 64, texture_size_y: 64),
+      Brick.new(self, x1: 600, x2: 728, y1: 120, y2: 312, texture: 'fire', texture_size_x: 32, texture_size_y: 32)
     ]
     @enemies = [
       Enemy.new(self, x: 50, y: 200, life: 1),
